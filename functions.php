8481c4af5cd1a209e8a8b87e43cf0e2848ba78e2
Alan Orth <alan.orth@gmail.com>
2016-02-26 12:06:01 +0200

    functions.php: Override parent theme's font URL function
    Twenty Sixteen uses fonts that don't have good coverage for Cyrillic
    script. We override them and use Fira Sans and Fira Mono instead.
    
    This includes a simple, verbatim port of all the parent's CSS rules
    that are incompatible. For example, we obviously override all references
    to the old font-families, but also, Fira Sans doesn't have a 900
    weight, so when replacing Merriweather we need to use 700 weight.
    
    Signed-off-by: Alan Orth <alan.orth@gmail.com>

--- functions.php
+++ functions.php
@@ -3,4 +3,33 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+/**
+ * Register Google fonts for Twenty Sixteen Cyrillic.
+ *
+ * @since Twenty Sixteen Cyrillic 0.9
+ *
+ * @return string Google fonts URL for the theme.
+ */
+function twentysixteen_fonts_url() {
+	$fonts_url = '';
+	$fonts     = array();
+	$subsets   = 'latin,latin-ext,cyrillic';
+
+	// Fira Sans for headers, body, etc.
+	$fonts[] = 'Fira+Sans:400,400italic,700,700italic';
+
+	// Fira Mono for monospace text.
+	$fonts[] = 'Fira+Mono';
+
+	if ( $fonts ) {
+		$fonts_url = add_query_arg( array(
+			'family' => urlencode( implode( '|', $fonts ) ),
+			'subset' => urlencode( $subsets ),
+		), 'https://fonts.googleapis.com/css' );
+	}
+
+	return $fonts_url;
+}
+
 ?>
